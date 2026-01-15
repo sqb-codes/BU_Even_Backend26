26ab9a1696aa5d13c2d31ab3bef8c5cd92d2e0c0
Ravikant Tyagi <150049400+sqb-codes@users.noreply.github.com>
2026-01-15 15:33:39 +0530

    Add files via upload

---
+++ Labs/Mon-React/first-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello with React</h1>
+      <h2>This is another tag</h2>
+    </>
+  )
+}
+
+export default App
---
+++ Labs/Thu-Lab/my-first-app/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  const username = "John";
+  return (
+    <h1>Welcome {username} React App...</h1>
+  )
+}
+
+export default App
